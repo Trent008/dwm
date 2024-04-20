350dd2d3a364c4ffc9e09208339f31cdd9a2f6f0
Trent008 <trenthekman68@gmail.com>
2024-04-20 16:08:24 -0500

    fix .Xresources

--- configuration/monitor.sh
+++ configuration/monitor.sh
@@ -1,2 +1,1 @@
-#!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x1200 --output HDMI-1 --auto --pos 1920x0
+
--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,7 @@
 cd ~
 sudo cp dwm/configuration/.xinitrc ~
 sudo cp dwm/configuration/settings.ini ~/.config/gtk-3.0
+sudo cp dwm/configuration/settings.ini /etc/gtk-3.0
 sudo cp dwm/configuration/.Xresources ~
 sudo cp dwm/configuration/.gtkrc-2.0 ~
 cd dwm
