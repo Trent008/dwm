83c60d23e6e552901571ba3f22a940efd7c22236
Trent008 <trenthekman68@gmail.com>
2024-04-20 16:30:57 -0500

    make theme application work

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,11 @@
 # installs dwm with my .xinitrc status display
 cd ~
 sudo cp dwm/configuration/.xinitrc ~
-sudo cp dwm/configuration/settings.ini ~/.config/gtk-3.0
 sudo cp dwm/configuration/settings.ini /etc/gtk-3.0
-sudo cp dwm/configuration/.Xresources ~
-sudo cp dwm/configuration/.gtkrc-2.0 ~
+mkdir ~/.config/gtk-3.0
+cp dwm/configuration/settings.ini ~/.config/gtk-3.0/settings.ini
+cp dwm/configuration/.Xresources ~
+cp dwm/configuration/.gtkrc-2.0 ~
 cd dwm
 sudo make clean install
 cd ~
