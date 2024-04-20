3e892f6c870dd337c75b5b0f5b1b1048bb37551d
Trent008 <trenthekman68@gmail.com>
2024-04-20 16:56:30 -0500

    improve theme application

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,11 @@
-# installs dwm with my .xinitrc status display
+# installs dwm with my .xinitrc status display and applies appearance config
 cd ~
 sudo cp dwm/configuration/.xinitrc ~
 sudo cp dwm/configuration/settings.ini /etc/gtk-3.0
-mkdir ~/.config/gtk-3.0
-cp dwm/configuration/settings.ini ~/.config/gtk-3.0/settings.ini
+mkdir .config/gtk-3.0
+mkdir .icons .icons/default
+cp dwm/configuration/settings.ini .config/gtk-3.0
+cp dwm/configuration/index.theme .icons/default
 cp dwm/configuration/.Xresources ~
 cp dwm/configuration/.gtkrc-2.0 ~
 cd dwm
