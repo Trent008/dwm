3d509b6e94a8581c89c104e827f52921a9cf449c
Trent008 <trenthekman6@gmail.com>
2024-04-20 13:05:59 -0500

    fix inconsistent cursor size

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,8 @@
 # installs dwm with my .xinitrc status display
 cd ~
-sudo echo "$(cat dwm/xinitrc)" > .xinitrc
+sudo cp dwm/xinitrc ~/.xinitrc
+sudo cp dwm/settings.ini /etc/gtk-3.0
+sudo echo "Xcursor.size: 32" > ~/.Xresources
 cd dwm
 sudo make clean install
 cd ~
