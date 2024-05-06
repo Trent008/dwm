62ed1ba42bdf372b0e094fc83bf3b526c7ecfbb6
Trent008 <trenthekman68@gmail.com>
2024-05-06 12:50:44 -0500

    mount google drive and one drive at startup

--- autostart.sh
+++ autostart.sh
@@ -2,4 +2,6 @@
 ./ultimate-system/monitor.sh &
 dunst &
 picom &
+rclone mount --daemon Dordt-OneDrive: /home/trent/Dordt-OneDrive &
+rclone mount --daemon "Google Drive:" /home/trent/Google-Drive &
 feh --bg-max ultimate-system/wallpapers/
