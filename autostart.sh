671f8f116d2f916bf79340941ca5c39bb7c10e9c
Trent008 <trenthekman68@gmail.com>
2024-05-07 10:00:06 -0500

    remove everything to do with vm

--- autostart.sh
+++ autostart.sh
@@ -2,7 +2,6 @@
 ./ultimate-system/monitor.sh &
 dunst &
 picom &
-swtpm socket --tpm2 --tpmstate dir=/home/trent/vm-tpm --ctrl type=unixio,path=/home/trent/vm-tpm/swtpm-sock &
 rclone mount --daemon Dordt-OneDrive: /home/trent/Dordt-OneDrive &
 rclone mount --daemon "Google Drive:" /home/trent/Google-Drive &
 feh --bg-max ultimate-system/wallpapers/
